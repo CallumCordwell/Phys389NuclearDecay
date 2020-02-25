bd73988fddb81ec9e62722c8cccc749b14cde9b2
Callum Cordwell <61294695+CallumCordwell@users.noreply.github.com>
2020-02-25 13:24:25 +0000

    Update MonteCarlo.py

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -38,6 +38,7 @@ def FuncX():
     """
     deltaT = 3000.0
 
+<<<<<<< Updated upstream
     lamda = decayConst('14C')
 
     P= lamda * deltaT
@@ -45,3 +46,50 @@ def FuncX():
     return P
 
 print(crudeMonteCarlo(10000))
+=======
+    Dconst = 0.00012096809
+    P= Dconst * deltaT
+
+    if x<=P:
+        return 1
+    else:
+        return 0
+
+def crudeVariance(sampleNum):
+    runningSum = 0.0
+
+    for i in range(sampleNum):
+        x = randomNumber(0,1)
+        runningSum+= f_of_x(x)**2
+    sumofsqs = runningSum*1/sampleNum
+    
+    runningSum=0.0
+    for i in range(sampleNum):
+        x = randomNumber(0,1)
+        runningSum+= f_of_x(x)
+    sqavg = (runningSum*1/sampleNum)**2
+
+    return  sumofsqs-sqavg
+
+
+t = 1000
+T=t
+N=10000
+Array = np.array([[0,N]])
+plt.plot(Array,Array)
+while N > 1:
+    N -= int(crudeMonteCarlo(N,t))
+    Array = np.append(Array,[[T,N]], axis=0)
+    T+=t
+
+#print(Array)   
+
+D = pd.DataFrame(Array, columns=('Time,y', 'Number of C14 nuclei'))
+print(D)
+D.to_csv('NucDecay.csv')
+plt.plot(Array[:,0],Array[:,1])
+plt.title('Decay of Carbon 14 nuclei using Monte Carlo method')
+plt.xlabel('Time, years')
+plt.ylabel('Number of C14 nuclei')
+plt.show()
+>>>>>>> Stashed changes
