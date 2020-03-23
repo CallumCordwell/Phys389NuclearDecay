0334116b89d4b56a5eb3403ae6cce66218cc52e6
Callum Cordwell <61294695+CallumCordwell@users.noreply.github.com>
2020-03-23 13:12:23 +0000

    Update MultiProc.py

--- MultiProc.py
+++ MultiProc.py
@@ -1,95 +1,64 @@
-from multiprocessing import Process, Queue
 import numpy as np
-from scipy import constants
 import math
-import pandas as pd
-import matplotlib.pyplot as plt
-import time
 import MonteCarlo as MC
-from ParticleClass import Nuclei, RadioNuclei
 import threading
+import queue
 
-start_time = time.time()
+queueLock = threading.Lock()
 
 
-Particles = np.array([])
-i=0
-while i<10:
-    Particles =np.append(Particles,[RadioNuclei('14C')])
-    i+=1
-
-Tend=5000
-tstep=1000
-MCNum=100
-energy = np.zeros((int((Tend/tstep) + 1),2), dtype=float)
-stability = np.zeros((int((Tend/tstep) + 1),2))
-"""
-if __name__ == '__main__':
-    q = Queue()
-    for i in range(MCNum):
-        p = Process(target=MC.Mutlitimestep, args=(Tend,Particles,t,q))
-        p.start()
-        TempEnergy , TempStability = q.get()
-        energy = np.add(energy, TempEnergy)
-        stability = np.add(stability,TempStability)
-        p.join()
-    print(energy)
-    print(stability)
-"""          
-        
 class myThread(threading.Thread):
-    def __init__(self, ID,q,Particles):
+    """
+    Class of processing threads, inherits functionality from Thread Class in threading module
+    Takes in all variables to run functions and defines as self varaibles
+    """
+    def __init__(self,ID,q,Particles,Tend,tstep):
         threading.Thread.__init__(self)
         self.id = ID
         self.tempenergy = np.array([[]])
         self.tempstability = np.array([[]])
         self.q=q
         self.Particles = Particles
+        self.Tend = Tend
+        self.tstep = tstep
     
     def run(self):
+        """
+        Primary use of myThread Class, runs the process which requires multithreadding
+        Called from start() function in threading.Thread class runs MonteCarloLoop for each thread
+        Fills queue with arrays from MonteCarloLoop
+        """
+        self.tempenergy, self.tempstability = MC.MonteCarloLoop(self.Tend,self.Particles,self.tstep)
         queueLock.acquire()
-        self.tempenergy, self.tempstability = MC.MonteCarloLoop(Tend,self.Particles,tstep)
-        
         self.q=self.q.put([self.tempenergy,self.tempstability])
         queueLock.release()
-        #process(self.id,self.q)
-
-def process(name,q):
-    queueLock.acquire()
-    print("proccessing " + name)
-    q.put(name)
-    queueLock.release()
-    time.sleep(1)
 
 
+def CreateandRunThreads(num,Tend,tstep,Particles):
+    """
+    Creates an array of threads objects for each Monte Carlo simulation and starts each thread
+    Takes in the number of simulations to run, end time for the simulation, and timesteps 
+    Outputs averaged data for each timestep summed over all simulations
+    """
+    q = queue.Queue()
+    threads =[]
+    energy = np.zeros((int((Tend/tstep) + 1),2), dtype=float)
+    stability = np.zeros((int((Tend/tstep) + 1),2))
+
+    for each in range(num):
+        thread = myThread(each,q,Particles,Tend,tstep)
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+        TempEnergy , TempStability = q.get()
+        energy = np.add(energy, TempEnergy)
+        stability = np.add(stability,TempStability)
+    return (energy/num), (stability/num)
 
 
-threadlist= ["thread1","thread2","thread3"]
-queueLock = threading.Lock()
-workQueue = Queue()
-threads =[]
-
-for each in threadlist:
-    thread = myThread(each,workQueue,Particles)
-    thread.start()
-    threads.append(thread)
 
 
-
-for t in threads:
-   t.join()
-   print(workQueue.get())
-print("Exiting Main Thread")
-
-
-"""
-    energy = np.add(energy,thread1.tempenergy)
-    stability = np.add(stability,thread2.tempstability)
-    energy = np.add(energy,thread2.tempenergy)
-    stability = np.add(stability,thread2.tempstability)
-
-"""
-
-print("--- %s seconds ---" % (time.time() - start_time))
 
 
