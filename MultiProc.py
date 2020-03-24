2e8fe6d2faa8c5f857f9c368b9d0d0807e85f559
Callum Cordwell <61294695+CallumCordwell@users.noreply.github.com>
2020-03-24 22:23:47 +0000

    Update MultiProc.py

--- MultiProc.py
+++ MultiProc.py
@@ -1,64 +1,75 @@
+from multiprocessing import Pool
+from scipy import constants
 import numpy as np
 import math
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
 import MonteCarlo as MC
-import threading
-import queue
+from ParticleClass import Nuclei, RadioNuclei
+from functools import partial
 
-queueLock = threading.Lock()
+def MyProcess(Tend,Particles,tstep,SimList):
+    func = partial(MC.MultiProcLoop, Tend,Particles,tstep)
+    with Pool() as pool:
+        p = pool.map(func,SimList)
+    return p
 
+def MultiProcRunning(Tend,Particles,tstep,MCNum):
+    pass
 
-class myThread(threading.Thread):
+def DataPlot(array):
     """
-    Class of processing threads, inherits functionality from Thread Class in threading module
-    Takes in all variables to run functions and defines as self varaibles
+    Plots a matplotlib graph for the given array
+    Takes in an array to output a pandas dataframe and a matplotlib graph
     """
-    def __init__(self,ID,q,Particles,Tend,tstep):
-        threading.Thread.__init__(self)
-        self.id = ID
-        self.tempenergy = np.array([[]])
-        self.tempstability = np.array([[]])
-        self.q=q
-        self.Particles = Particles
-        self.Tend = Tend
-        self.tstep = tstep
-    
-    def run(self):
-        """
-        Primary use of myThread Class, runs the process which requires multithreadding
-        Called from start() function in threading.Thread class runs MonteCarloLoop for each thread
-        Fills queue with arrays from MonteCarloLoop
-        """
-        self.tempenergy, self.tempstability = MC.MonteCarloLoop(self.Tend,self.Particles,self.tstep)
-        queueLock.acquire()
-        self.q=self.q.put([self.tempenergy,self.tempstability])
-        queueLock.release()
+    D = pd.DataFrame(array)
+    D.to_csv('NucDecay.csv')
+
+    plt.plot(array[:,0] , array[:,1])
+    plt.title('Average decay energy released for 10 atoms of C14 decay')
+    plt.xlabel('Time, y')
+    plt.ylabel('Average decay energy release')
+    plt.show()
+
+def startUp():
+    Particles = np.array([])
+    i=0
+    while i<10:
+        Particles =np.append(Particles,[RadioNuclei('14C')])
+        i+=1
+
+    MCNum = 50000
+    tstep=100
+    Tend=60000
+
+    return MCNum,tstep,Tend,Particles
 
 
-def CreateandRunThreads(num,Tend,tstep,Particles):
-    """
-    Creates an array of threads objects for each Monte Carlo simulation and starts each thread
-    Takes in the number of simulations to run, end time for the simulation, and timesteps 
-    Outputs averaged data for each timestep summed over all simulations
-    """
-    q = queue.Queue()
-    threads =[]
+
+
+if __name__ ==  "__main__":
+    start_time = time.time()
+
+    MCNum,tstep,Tend,Particles = startUp()
     energy = np.zeros((int((Tend/tstep) + 1),2), dtype=float)
     stability = np.zeros((int((Tend/tstep) + 1),2))
+    print("Starting Threading")
+    SimList = [x for x in range(0,MCNum)]
+    
+    pool = np.array(MyProcess(Tend,Particles,tstep,SimList))
+    
+    stablepool = pool[:,0]
+    energypool = pool[:,1]
+    for each in stablepool:
+        stability = np.add(stability,each)
+    for each in energypool:
+        energy = np.add(energy,each)
 
-    for each in range(num):
-        thread = myThread(each,q,Particles,Tend,tstep)
-        thread.start()
-        threads.append(thread)
+    print(stability/MCNum)
+    print(energy/MCNum)
+    
+    print("Exiting Main Thread. Time: %s seconds" % (time.time() - start_time))
 
-    for thread in threads:
-        thread.join()
-        TempEnergy , TempStability = q.get()
-        energy = np.add(energy, TempEnergy)
-        stability = np.add(stability,TempStability)
-    return (energy/num), (stability/num)
-
-
-
-
-
+    DataPlot(stability/MCNum)
 
