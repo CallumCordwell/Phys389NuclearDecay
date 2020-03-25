0acac87da7373f521a2947c8d43e15d8aa76ccae
Callum Cordwell <61294695+CallumCordwell@users.noreply.github.com>
2020-03-25 11:33:25 +0000

    Update Runner.py
    Merged with RunnerMK2.py as MK2 did the same function more elegantly

--- Runner.py
+++ Runner.py
@@ -7,73 +7,54 @@ import time
 import MonteCarlo as MC
 from ParticleClass import Nuclei, RadioNuclei
 
+def dataPlot(array):
+    """
+    Plots a matplotlib graph for the given array
+    Takes in an array to output a pandas dataframe and a matplotlib graph
+    """
+    D = pd.DataFrame(array)
+    #print(Stability)
+    #DataArray = np.append(DataArray, EndEnergy[:,None] ,axis=1)
+    D.to_csv('NucDecay.csv')
+
+    plt.plot(array[:,0] , array[:,1])
+    plt.title('Average decay energy released for 10 atoms of C14 decay')
+    plt.xlabel('Time, y')
+    plt.ylabel('Average decay energy release')
+    plt.show()
+
+"""
+Runs the monte carlo sim without multiprocessing using MonteCarloLoop
+"""
+
+
 start_time = time.time()
 
-def reset():
-    global Particles
-    global Names
-    global DEnergy
-    global Energy
-    DEnergy = 0
-    i = 0
+
+MCNum = 100
+tstep=1000
+Tend=5000
+Energy = np.zeros((int(Tend/tstep +1),2), dtype=float)
+stability = np.zeros((int(Tend/tstep +1),2))
+
+
+for i in range(MCNum):
     Particles = np.array([])
-    Names = np.array([])
-    Energy = np.array([])
-    while i<10:
+    for i in range(10):
         Particles =np.append(Particles,[RadioNuclei('14C')])
-        i+=1
-
-t = 1000
-Tend = 10000
-EndEnergy = np.array([])
-Stability = np.zeros([(int(Tend/t)+1),2]) #+1 as the time step starts at 0, remove if starting at T=t
-
-N=0
-NT=200
-DataArray = np.empty([10])
-DataArray = [DataArray]
-while N<NT:
-    reset()
-    T=0
     
-    while T<=Tend:
-        instability=0
-        if not T==0:
-            Particles, DEnergy = MC.timestep(t,Particles)
-        else:
-            DEnergy = 0
-        array = np.array([])
-        for cell in Particles:
-            array = np.append(array, cell.name)
-            if not cell.stable:
-                instability+=1
-        DataArray = np.append(DataArray,[array],axis=0)
-        Energy = np.append(Energy,DEnergy)
-        Stability[int(T/t)][1] = T
-        Stability[int(T/t)][0] += instability
-        T+=t
+    DEnergy, LoopStability = MC.MonteCarloLoop(Tend,Particles,tstep)
 
-    #End of each Monte Carlo loop data
-    EndEnergy = np.append(EndEnergy, np.sum(Energy))
-    N+=1
+    stability = np.add(stability,LoopStability)
+    Energy = np.add(Energy, DEnergy)
 
+stability /= MCNum
+Energy /= MCNum
 
-print(Stability)
+EndEnergy = np.sum(Energy[:,1])
 
-DataArray = np.delete(DataArray,0,0)
-Stability[:,0] /=NT
-
-D = pd.DataFrame(Stability)
-#print(Stability)
-#DataArray = np.append(DataArray, EndEnergy[:,None] ,axis=1)
-D.to_csv('NucDecay.csv')
-
-plt.plot(Stability[:,1] , Stability[:,0])
-plt.title('Average number of stable particles over time for C14 decay')
-plt.xlabel('Time, y')
-plt.ylabel('Average number of stable particles')
-plt.show()
-
-
+print(Energy)
+print(stability)
+dataPlot(stability)
 
 print("--- %s seconds ---" % (time.time() - start_time))
