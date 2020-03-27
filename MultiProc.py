2ed3a555c9badaae0fcfd214120041519c51aac2
Callum Cordwell <61294695+CallumCordwell@users.noreply.github.com>
2020-03-27 09:52:58 +0000

    Update MultiProc.py

--- MultiProc.py
+++ MultiProc.py
@@ -8,6 +8,7 @@ import time
 import MonteCarlo as MC
 from ParticleClass import Nuclei, RadioNuclei
 from functools import partial
+import Functions as functions
 
 def MyProcess(Tend,Particles,tstep,SimList):
     """
@@ -21,35 +22,17 @@ def MyProcess(Tend,Particles,tstep,SimList):
         p = pool.map(func,SimList)
     return p
 
-def dataPlot(array):
-    """
-    Plots a matplotlib graph for the given array, assumes the first column is the x axis and the second is the y axis
-    Requires manual change for axis names and graph title
-    Takes in an array to output a pandas dataframe and a matplotlib graph
-    """
-    D = pd.DataFrame(array)
-    D.to_csv('NucDecay.csv')
-
-    plt.plot(array[:,0] , array[:,1])
-    plt.title('Average decay energy released for 10 atoms of C14 decay')
-    plt.xlabel('Time, y')
-    plt.ylabel('Average decay energy release')
-    plt.show()
-
 def startUp():
     """
     Defines the variables for use in the multiprocessing processes
     returns all the variables needed to run the Monte Carlo sims such as time steps and number of sims to be run 
     """
-    Particles = np.array([])
-    i=0
-    while i<10:
-        Particles =np.append(Particles,[RadioNuclei('14C')])
-        i+=1
+    ToBeMade = np.array([["10","14C"],["10","231PA"],["5","212BI"],["20","81KR"],["20","85KR"]])
+    Particles = functions.CreateParticles(ToBeMade)
 
-    MCNum = 50
-    tstep=100
-    Tend=6000
+    MCNum = 10000
+    tstep=20
+    Tend=150000
 
     return MCNum,tstep,Tend,Particles
 
@@ -60,7 +43,7 @@ if __name__ ==  "__main__":
     MCNum,tstep,Tend,Particles = startUp()
     energy = np.zeros((int((Tend/tstep) + 1),2), dtype=float)
     stability = np.zeros((int((Tend/tstep) + 1),2))
-
+    
 
     print("Starting Threading")
     SimList = [x for x in range(0,MCNum)]
@@ -79,5 +62,6 @@ if __name__ ==  "__main__":
     
     print("Exiting Main Thread. Time: %s seconds" % (time.time() - start_time))
 
-    dataPlot(stability/MCNum)
+    functions.dataPlot(stability/MCNum, "StabiltyRan")
+    functions.dataPlot(energy/MCNum,"EnergyRan")
 
