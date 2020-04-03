5a42609283408c88104ed91348e367674cff5461
Callum Cordwell <61294695+CallumCordwell@users.noreply.github.com>
2020-04-03 15:57:39 +0100

    Update MonteCarlo.py

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -25,9 +25,10 @@ def crudeMonteCarlo(sampleNum, t ):
     runningTot = 0.0
 
     for i in range(sampleNum):
-        runningTot += Probability(t)
+        if Probability(t):
+            runningTot += 1
 
-    return float(runningTot)
+    return runningTot
 
 
 def Probability(deltaT):
@@ -41,35 +42,17 @@ def Probability(deltaT):
     P= Dconst * deltaT
 
     if x<=P:
-        return 1
+        return True
     else:
-        return 0
+        return False
 
-def Variance(sampleNum):
-    """
-    Legacy code to find the variance of a function f_of_x
-    Unusable in this simulation given the true/false nature of the experiment so a different way to calculate vairance is needed 
-    """
-    runningSum = 0.0
-
-    for i in range(sampleNum):
-        x = randomNumber(0,1)
-        runningSum+= f_of_x(x)**2
-    sumofsqs = runningSum*1/sampleNum
-    
-    runningSum=0.0
-    for i in range(sampleNum):
-        x = randomNumber(0,1)
-        runningSum+= f_of_x(x)
-    sqavg = (runningSum*1/sampleNum)**2
-
-    return  sumofsqs-sqavg
 
 
 def ObjectMonteCarlo(sample, t ):
     """
-    A variant of the Monte Carlo simulation designed to run using a Nuclei object
+    A variant of the curde Monte Carlo function designed to run using a Nuclei object
     Uses the decay constant of the Nuclei and the random number generator to determine whether it decays
+    Returns a boolean true if the nuclei decays
     """
     
     x = randomNumber(0,1)
@@ -87,9 +70,9 @@ def decayloop(Nucleus, t):
     """
     if not Nucleus.stable:
         if ObjectMonteCarlo(Nucleus,t):
-            Nucleus , DEnergy = Nucleus.decay()
-            return Nucleus , DEnergy
-    return Nucleus , 0
+            Nucleus , DEnergy, DecayNum = Nucleus.decay()
+            return Nucleus , DEnergy, DecayNum
+    return Nucleus , 0 , 0
 
 def timestep(t,Particles):
     """
@@ -99,93 +82,50 @@ def timestep(t,Particles):
     """
     i=0
     DEnergy = 0
+    DecayNum = 0
     newArray=Particles
-    for cell in Particles:
-        newArray[i], DE = decayloop(cell,t)
+    for cell in newArray:
+        newArray[i], DE ,DN = decayloop(cell,t)
         DEnergy +=DE
+        DecayNum +=DN
         i+=1
     
-    return newArray, DEnergy
+    return newArray, DEnergy,DecayNum
 
 
-def StandardDeviationAnalysis():
+def MonteCarloLoop(Tend,Particles,tstep,SimNum=0):
     """
-    Function to analyse the standard deviation of the random number generator used
-    Produces a pandas dataframe and a matplotlib graph of the range of random numbers produced.
-    """
-    array = np.array([])
-    i=0
-    j=0
-    x=0
-    while j<1000:
-        while  i<100:
-            x+= Probability(3000)
-            
-            i+=1
-        array = np.append(array,x/1000)
-        x=0
-        j+=1
-        i=0
-
-    D = pd.DataFrame(array)
-    D.to_csv('randomnumberanalysis.csv')
-
-    plt.plot(array)
-    plt.title('Probability estimation for 100 attempts')
-    plt.xlabel('Event number')
-    plt.ylabel('Average number of passes')
-    plt.show()
-
-
-def MonteCarloLoop(Tend,Particles,tstep):
-    """
-    Function to run a Monte Carlo simulation over a given period for an array of nuclei#
+    Function to run a Monte Carlo simulation over a given period for an array of nuclei
     Takes in end time timit, the timestep, and an array of nuclei to run the checks per timestep
     Assumes at T=0 all the particles are as inputted and Delta E = 0
+    SimNum is the number of the simulation being run, this is not used but the multiprocessing pool requires to send an iterable list of numbers
+    Returns the array of unstable particle number, cummulative energy released at each timestep, and  cummulative number of decays at each timestep
     """
     Energy = np.zeros((1,2))
     UnstableNum = np.array([[0,Particles.size]])
+    Decays = np.zeros((1,2))
 
     TotalEnergy = 0
+    TotalDecays = 0
     T=tstep
 
     while T<=Tend:
+        if T==tstep:
+            for each in Particles:
+                if each.stable:
+                    print(np.where(Particles == each))
+                    print(each.name)
+        
         instability = 0
-        Particles, DEnergy = timestep(tstep,Particles)
+        Particles, DEnergy , DecayNum = timestep(tstep,Particles)
         TotalEnergy +=DEnergy
+        TotalDecays +=DecayNum
         for cell in Particles:
             if not cell.stable:
                 instability+=1
 
         Energy = np.append(Energy,[[T,TotalEnergy]],axis=0)
         UnstableNum = np.append(UnstableNum,[[T,instability]],axis=0)
+        Decays = np.append(Decays, [[T,TotalDecays]],axis=0)
         T+=tstep
-    return Energy , UnstableNum
-
-def MultiProcLoop(Tend,Particles,tstep,SimList):
-    """
-    Similar to the MonteCarloLoop function will run a full monte carlo simmulation over a given time period
-    Created to be run alongside the multiprocessing function in MultiProc.py
-    Takes in Tend,Particles and tstep to run the monte carlo simulation every tstep until Tend on the nuclei in particles array
-    SimList is a list of 0 to the number of Monte Carlo sims to be run, not used but the multiprocessing pool requires to send an iterable of numbers
-    Returns the array of unstable particle number and cummulative energy released at each timestep 
-    """
-    
-    Energy = np.zeros((1,2))
-    UnstableNum = np.array([[0,Particles.size]])
-    T=tstep
-    TempParticles = np.array(Particles)
-    
-    TotalEnergy = 0
-    while T<=Tend:
-        TempParticles, DEnergy = timestep(tstep,TempParticles)
-        
-        instability = 0
-        for cell in TempParticles:
-            if not cell.stable:
-                instability+=1
-        TotalEnergy +=DEnergy
-        Energy = np.append(Energy,[[T,TotalEnergy]],axis=0)
-        UnstableNum = np.append(UnstableNum,[[T,instability]],axis=0)
-        T+=tstep
-    return UnstableNum, Energy
+    return Energy , UnstableNum, Decays
